c2fd4c24dbfaa4dfec7da08faf756b7f9e551dc4
joannajjliu <joanna.liu3774@gmail.com>
2020-04-16 23:25:08 -0400

    class, access modifiers, interfaces

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 "use strict";
 // benefits of ts: static type checking, accurate intellisense
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 exports.__esModule = true;
 var message = "Welcome back";
 console.log(message);
@@ -50,3 +63,70 @@ function hasName(obj) {
         typeof obj === "object" &&
         "name" in obj;
 }
+if (hasName(myVariable)) {
+    console.log(myVariable.name);
+}
+// Type inference:
+var a;
+a = 10;
+a = true;
+var b = 20;
+// b = true; //has type error (due to type inference)
+// Union types
+var multiType;
+multiType = 20;
+multiType = true;
+// Typescript functions
+function add(num1, num2) {
+    if (num1 === void 0) { num1 = 6; }
+    //optional params must always be after required params
+    //num1 has a default, num2 is optional
+    var totalSum = num1 + num2;
+    console.log(totalSum);
+    return totalSum;
+}
+add(5, 10);
+add(5);
+;
+function fullname(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+;
+var p = {
+    firstName: 'Bruce',
+    lastName: 'Wayne'
+};
+fullname(p);
+// ts Classes (like in ES6, C#, and Java)
+var Employee = /** @class */ (function () {
+    function Employee(name) {
+        this.employeeName = name;
+    }
+    Employee.prototype.greet = function () {
+        console.log("Good morning " + this.employeeName);
+    };
+    return Employee;
+}());
+var emp1 = new Employee("Baedee");
+console.log(emp1.employeeName);
+emp1.greet();
+// class inheritance:
+var Manager = /** @class */ (function (_super) {
+    __extends(Manager, _super);
+    function Manager(managerName) {
+        return _super.call(this, managerName) || this;
+    }
+    Manager.prototype.delegateWork = function () {
+        console.log("Manager " + this.employeeName + " delegates tasks");
+    };
+    return Manager;
+}(Employee));
+var m1 = new Manager('Bello');
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
+// access modifiers (keywords setting accessibility of class properties and methods):
+//public -> default, each class property is public, and can be freely accessed throughout program
+//private -> property only accessible within own class; not accessible by derived class, nor outside call
+//protected -> property accessible by derived class (this.employeeName can be used in Manager class), 
+//              but not via outside call (m1.employeeName DOSEN'T work)
--- main.ts
+++ main.ts
@@ -2,6 +2,9 @@
 
 //"tsc main.ts" in terminal to add a js file
 // "tsc main --watch" will watch ts file for changes and recreate js file as needed
+// "node main" to run js file
+// "nodemon main" to continue running js file, and watch for changes
+
 export {}
 let message = "Welcome back";
 console.log(message);
@@ -60,3 +63,88 @@ function hasName (obj: any): obj is {name: string} {
         typeof obj === "object" &&
         "name" in obj
 }
+if (hasName(myVariable)) {
+    console.log(myVariable.name);
+}
+
+// Type inference:
+let a;
+a = 10;
+a = true;
+
+let b = 20;
+// b = true; //has type error (due to type inference)
+
+// Union types
+let multiType: number | boolean;
+multiType = 20;
+multiType = true;
+
+// Typescript functions
+function add(num1: number = 6, num2?: number): number {
+    //optional params must always be after required params
+    //num1 has a default, num2 is optional
+    let totalSum: number = num1 + num2;
+    console.log(totalSum);
+    return totalSum;
+}
+add(5, 10);
+add(5);
+
+// Object type
+interface Person {
+    firstName: string;
+    lastName?: string; //lastName is an optional property
+};
+
+function fullname(person: Person) {
+    console.log(`${person.firstName} ${person.lastName}`);
+};
+
+let p = {
+    firstName: 'Bruce',
+    lastName: 'Wayne'
+};
+
+fullname(p);
+
+// ts Classes (like in ES6, C#, and Java)
+
+class Employee { // Employee class
+    employeeName: string; // class property -> default is public property
+
+    constructor(name: string) { // constructor
+        this.employeeName = name;
+    }
+
+    greet() { // method
+        console.log(`Good morning ${this.employeeName}`);
+    }
+}
+
+let emp1 = new Employee("Baedee");
+console.log(emp1.employeeName);
+emp1.greet();
+
+// class inheritance:
+class Manager extends Employee {
+    constructor(managerName: string) {
+        super(managerName); //super call to the base class constructor
+    }
+    delegateWork() {
+        console.log(`Manager ${this.employeeName} delegates tasks`);
+    }
+}
+
+let m1 = new Manager('Bello');
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
+
+// access modifiers (keywords setting accessibility of class properties and methods):
+//public -> default, each class property is public, and can be freely accessed throughout program
+
+//private -> property only accessible within own class; not accessible by derived class, nor outside call
+
+//protected -> property accessible by derived class (this.employeeName can be used in Manager class), 
+//              but not via outside call (m1.employeeName DOSEN'T work)
